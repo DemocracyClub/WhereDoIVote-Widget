3bf44fc999ccd9363ffc1d040a63d37e5c7d0f2f
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2017-06-03 00:51:40 +0100

    Add tests for StationNotFound

--- src/StationNotFound.js
+++ src/StationNotFound.js
@@ -5,19 +5,20 @@ function StationNotFound(props) {
         <div className="democracy_club_embed">
             <div className="card">
                 <h2>We couldn't find your station</h2>
-                <div>
-                    Get in touch with <b>
-                    { props.council.council_id.startsWith('N09') ? 'The Electoral Office for Northern Ireland' : props.council.name }</b>:
-                     <br/>
-                     <br/>
-                     <ul>
-                        <li>Website - <a href={props.council.website}>{props.council.website}</a></li>
-                        <li>Phone - {props.council.phone}</li>
-                        <li>Email - <a href={"mailto:"+props.council.email}>{props.council.email}</a></li>
-                     </ul>
-                     <br/>
-
-                 </div>
+                { props.council &&
+                    <div>
+                        <span>Get in touch with <b>
+                        { props.council.council_id.startsWith('N09') ? 'The Electoral Office for Northern Ireland' : props.council.name }</b>:</span>
+                         <br/>
+                         <br/>
+                         <ul>
+                            <li>Website - <a href={props.council.website}>{props.council.website}</a></li>
+                            <li>Phone - {props.council.phone}</li>
+                            <li>Email - <a href={"mailto:"+props.council.email}>{props.council.email}</a></li>
+                         </ul>
+                         <br/>
+                     </div>
+                 }
                  <button href="#" onClick={props.home}>Back to postcode search</button>
 
                 <div>
---
+++ src/StationNotFound.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import 'jest-enzyme';
+import StationNotFound from './StationNotFound';
+
+const council = { council_id: 'test', name: 'Example Council' };
+
+describe('StationNotFound', () => {
+    it('always renders header', () => {
+        const wrapper = shallow(<StationNotFound />);
+
+        expect(wrapper).toContainReact(<h2>We couldn't find your station</h2>);
+    });
+
+    it('should present council to get in touch with', () => {
+        const wrapper = shallow(<StationNotFound council={council}/>);
+
+        expect(wrapper).toContainReact(<span>Get in touch with <b>Example Council</b>:</span>);
+    });
+
+    it('should present NI Electoral Office for N09 postcodes', () => {
+        const council = { council_id: 'N09 1XA' }
+        const wrapper = shallow(<StationNotFound council={council}/>);
+
+        expect(wrapper).toContainReact(<span>Get in touch with <b>The Electoral Office for Northern Ireland</b>:</span>);
+    });
+
+    describe('should present contact details', () => {
+        const council = { council_id: 'N09 1XA', website: 'example.com', phone: '118 118', email: 'test@example.com' }
+
+        it('via website', () => {
+            const wrapper = shallow(<StationNotFound council={council}/>)
+
+            expect(wrapper).toContainReact(<li>Website - <a href="example.com">example.com</a></li>);
+        });
+
+        it('via phone', () => {
+            const wrapper = shallow(<StationNotFound council={council}/>)
+
+            expect(wrapper).toContainReact(<li>Phone - 118 118</li>);
+        });
+
+        it('via email', () => {
+            const wrapper = shallow(<StationNotFound council={council}/>)
+
+            expect(wrapper).toContainReact(<li>Email - <a href="mailto:test@example.com">test@example.com</a></li>);
+        });
+    });
+});
