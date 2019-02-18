093b2098313c1a05b255b65688140974e493178f
Alex Wilson <alex+github@probablyfine.co.uk>
2019-02-18 19:28:45 +0000

    feat(analytics): Report full embed url in tracking params
    * This is a change from reporting only the hostname
    * The parameter will be escaped to avoid issues parsing special characters in URLs

--- src/WdivAPI.js
+++ src/WdivAPI.js
@@ -13,8 +13,10 @@ function API(client) {
     }
 
     function addAnalytics() {
-        if (window && window.location && window.location.hostname && window.location.hostname !== "") {
-            return "utm_source="+window.location.hostname+"&utm_medium=widget";
+        if (window && window.location && window.location.href && window.location.href !== "") {
+            let encodedUrl = encodeURIComponent(window.location.href);
+
+            return "utm_source="+encodedUrl+"&utm_medium=widget";
         } else {
             return "utm_source=unknown&utm_medium=widget";
         }
