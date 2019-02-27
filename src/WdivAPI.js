ea3ac5c41197d32dc4028a156958905a2b2e5a9a
Alex Wilson <alex+github@probablyfine.co.uk>
2019-02-27 16:29:04 +0000

    refactor(api): Use try/catch instead of triple-if condition for readability

--- src/WdivAPI.js
+++ src/WdivAPI.js
@@ -13,12 +13,11 @@ function API(client) {
     }
 
     function addAnalytics() {
-        if (window && window.location && window.location.href && window.location.href !== "") {
-            let encodedUrl = encodeURIComponent(window.location.href);
-
-            return "utm_source="+encodedUrl+"&utm_medium=widget";
-        } else {
-            return "utm_source=unknown&utm_medium=widget";
+        try {
+          const encodedUrl = encodeURIComponent(window.location.href)
+          return "utm_source="+encodedUrl+"&utm_medium=widget";
+        } catch (e) {
+          return "utm_source=unknown&utm_medium=widget";
         }
     }
 
