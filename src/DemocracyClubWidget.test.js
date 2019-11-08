30d2dd37c1d8e8f1de46c11f403f329bd64dafba
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-11-08 16:21:57 +0000

    Test reset widget button

--- src/DemocracyClubWidget.test.js
+++ src/DemocracyClubWidget.test.js
@@ -1,4 +1,4 @@
-import { waitForElement } from '@testing-library/react';
+import { waitForElement, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import cy_messages from './translations/cy';
 import en_messages from './translations/en';
@@ -7,6 +7,7 @@ import {
   renderEnglishWidget,
   typePostcode,
   submitPostcode,
+  mockResponse,
 } from './test-utils/test';
 
 jest.mock(`!!raw-loader!./widget-styles.css`, () => '.DCWidget {margin: 0; }', {
@@ -35,8 +36,11 @@ describe('WhereDoIVote Welsh Widget', () => {
 });
 
 describe('WhereDoIVote English Widget', () => {
+  let getByLabelText, getByTestId;
   beforeEach(async () => {
-    renderEnglishWidget();
+    const wrapper = renderEnglishWidget();
+    getByLabelText = wrapper.getByLabelText;
+    getByTestId = wrapper.getByTestId;
   });
 
   it('should give error message when no postcode is entered', async () => {
@@ -52,5 +56,16 @@ describe('WhereDoIVote English Widget', () => {
     submitPostcode();
     const newContent = await waitForElement(() => document.querySelector('#dc_error'));
     expect(newContent).toHaveTextContent(en_messages['postcode.errors.invalid-postcode']);
+  });
+
+  it('should allow user to start again', async () => {
+    let enteredPostcode = 'AA11AA';
+    mockResponse('postcode', enteredPostcode);
+    typePostcode(enteredPostcode);
+    submitPostcode();
+    await waitForElement(() => document.querySelector('.NoUpcomingElection'));
+    fireEvent.click(getByLabelText(en_messages['general.start-again']));
+    const EnterPostcode = await waitForElement(() => getByTestId('postcode-selector'));
+    expect(EnterPostcode).toHaveTextContent(en_messages['postcode.enter-postcode']);
   });
 });
