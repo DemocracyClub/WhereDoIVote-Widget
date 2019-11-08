6c4a747c0fa97116fcae5fc7eda020e97bce630c
Alasdair Blackwell <aliblackwell@protonmail.com>
2019-11-08 11:00:43 +0000

    Add language data attribute

--- src/withTranslations.js
+++ src/withTranslations.js
@@ -5,12 +5,14 @@ import messages_en from './translations/en.json';
 import messages_cy from './translations/cy.json';
 
 function withTranslations(Widget) {
+  let languageConfig = document.getElementById('dc_wdiv').getAttribute('data-language');
+
   return function WidgetWithTranslations(props) {
     const messages = {
       en: messages_en,
       cy: messages_cy,
     };
-    const [language, setLanguage] = useState('en');
+    const [language, setLanguage] = useState(languageConfig);
     return (
       <IntlProvider locale={language} messages={messages[language]}>
         <Widget {...props} setLanguage={setLanguage} locale={language} />
