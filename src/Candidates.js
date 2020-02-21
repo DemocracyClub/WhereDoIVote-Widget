7f1bff41b258308aecf6d2342204ce7dbe0744ce
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-02-21 16:38:30 +0000

    List type elections

--- src/Candidates.js
+++ src/Candidates.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import CandidateItem from './CandidateItem';
+import BallotTypeList from './BallotTypeList';
 
 function Candidates(props) {
   return (
-    <section className="Candidates" data-testid="candidates">
-      <ul>
-        {props.ballot.candidates.map((candidate, i) => (
-          <CandidateItem candidate={candidate} key={`candidate-${i}`} />
-        ))}
-      </ul>
-    </section>
+    <>
+      {!props.ballot.election_id.includes('gla.a') && (
+        <section className="Candidates" data-testid="candidates">
+          <ul>
+            {props.ballot.candidates.map((candidate, i) => (
+              <CandidateItem candidate={candidate} key={`candidate-${i}`} />
+            ))}
+          </ul>
+        </section>
+      )}
+      {props.ballot.election_id.includes('gla.a') && <BallotTypeList {...props} />}
+    </>
   );
 }
 
