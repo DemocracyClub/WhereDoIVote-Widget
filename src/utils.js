e7eb326ea4843e1b88a87d9df68b0ae56d8f1641
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-02-26 11:26:53 +0000

    Add words for numbers fixes #413

---
+++ src/utils.js
@@ -0,0 +1,38 @@
+function getWordsFromNumber(n, translations) {
+  let word;
+  switch (n) {
+    case 1:
+      word = translations['numbers.one'];
+      break;
+    case 2:
+      word = translations['numbers.two'];
+      break;
+    case 3:
+      word = translations['numbers.three'];
+      break;
+    case 4:
+      word = translations['numbers.four'];
+      break;
+    case 5:
+      word = translations['numbers.five'];
+      break;
+    case 6:
+      word = translations['numbers.six'];
+      break;
+    case 7:
+      word = translations['numbers.seven'];
+      break;
+    case 8:
+      word = translations['numbers.eight'];
+      break;
+    case 9:
+      word = translations['numbers.nine'];
+      break;
+    case 10:
+      word = translations['numbers.ten'];
+      break;
+  }
+  return word;
+}
+
+export default getWordsFromNumber;
