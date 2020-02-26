eb65dfff73bf8a1ee90dcd69f2b625fe977c366b
Alasdair Blackwell <aliblackwell@protonmail.com>
2020-02-26 13:34:50 +0000

    Show all dates #418

--- src/withCandidates.js
+++ src/withCandidates.js
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function withCandidates(Widget) {
   return function WidgetWithCandidates(props) {
     const renderTarget = document.getElementById('dc_wdiv');
     const enableCandidates = renderTarget.getAttribute('data-candidates') === 'true' ? true : false;
-    const [ballots, setBallots] = useState(null);
 
-    function handleCandidates(nextBallotDate) {
-      if (enableCandidates && nextBallotDate) {
-        setBallots(nextBallotDate.ballots);
-      }
-    }
-
-    return (
-      <Widget
-        {...props}
-        enableCandidates={enableCandidates}
-        handleCandidates={handleCandidates}
-        resetBallot={() => setBallots(null)}
-        ballots={ballots}
-      />
-    );
+    return <Widget {...props} enableCandidates={enableCandidates} />;
   };
 }
 
