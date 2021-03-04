bf4bfa1782c79b10df5eec0c63c9afa82b43ad1c
Virginia Dooley <vcdooley@gmail.com>
2021-03-04 17:19:57 +0000

    Test to conditionally remove links to DC site from CandidateList

--- src/CandidateList.js
+++ src/CandidateList.js
@@ -4,28 +4,50 @@ import { injectIntl } from 'react-intl';
 function CandidateList(props) {
   const { formatMessage } = props.intl;
   const { candidates } = props;
-  return (
-    <ul>
-      {candidates.map((candidate) => (
-        <li className="CandidateListItem" key={candidate.person.ynr_id}>
-          <h4 className={`candidate-name party-${candidate.party.party_id.split(':')[1]}`}>
-            <a
-              data-testid="candidate-link"
-              title={formatMessage({ id: 'general.read-more-info-candidate' })}
-              href={candidate.person.absolute_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {candidate.person.name}
-            </a>
-          </h4>{' '}
-          <h5 className={`party-name party-${candidate.party.party_id.split(':')[1]}`}>
-            {candidate.party.party_name}
-          </h5>
-        </li>
-      ))}
-    </ul>
-  );
+
+  function handleCandidateList(candidates) {
+    if (process.env.REACT_APP_API === 'ec') {
+      return (
+        <div>
+          {candidates.map((candidate) => (
+            <li className="CandidateListItem" key={candidate.person.ynr_id}>
+              <h4 className={`candidate-name party-${candidate.party.party_id.split(':')[1]}`}>
+                {candidate.person.name}
+              </h4>{' '}
+              <h5 className={`party-name party-${candidate.party.party_id.split(':')[1]}`}>
+                {candidate.party.party_name}
+              </h5>
+            </li>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {candidates.map((candidate) => (
+            <li className="CandidateListItem" key={candidate.person.ynr_id}>
+              <h4 className={`candidate-name party-${candidate.party.party_id.split(':')[1]}`}>
+                <a
+                  data-testid="candidate-link"
+                  title={formatMessage({ id: 'general.read-more-info-candidate' })}
+                  href={candidate.person.absolute_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {candidate.person.name}
+                </a>
+              </h4>{' '}
+              <h5 className={`party-name party-${candidate.party.party_id.split(':')[1]}`}>
+                {candidate.party.party_name}
+              </h5>
+            </li>
+          ))}
+        </div>
+      );
+    }
+  }
+
+  return <ul>{handleCandidateList(candidates)}</ul>;
 }
 
 export default injectIntl(CandidateList);
