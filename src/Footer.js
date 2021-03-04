9c317ad5177a8492dbc36538e6d845d5cc5da2e4
Virginia Dooley <vcdooley@gmail.com>
2021-03-04 14:47:22 +0000

    Add conditional logo on footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { BuiltByDC } from './Branding';
 import LanguageToggle from './LanguageToggle';
 
+function handleLogo(props) {
+  if (process.env.REACT_APP_API === 'mock_ec') {
+    return (
+      <footer>
+        <img
+          src={'https://ukelectoralcommission.files.wordpress.com/2014/03/logo2.png'}
+          alt="Electoral Commission Logo"
+          width={'100px'}
+        />
+      </footer>
+    );
+  } else {
+    return (
+      <footer>
+        {props.showLanguageToggle && <LanguageToggle {...props} />}
+        <BuiltByDC />
+      </footer>
+    );
+  }
+}
+
 function Footer(props) {
-  return (
-    <footer>
-      {props.showLanguageToggle && <LanguageToggle {...props} />}
-      <BuiltByDC />
-    </footer>
-  );
+  return <footer>{handleLogo(props)}</footer>;
 }
 
 export default Footer;
