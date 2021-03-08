6f775aa1fbca98a0ac018b339b13bca450734870
Virginia Dooley <vcdooley@gmail.com>
2021-03-08 11:52:18 +0000

    remove conditional in footer and moved to brand component

--- src/Footer.js
+++ src/Footer.js
@@ -1,31 +1,36 @@
 import React from 'react';
-import { BuiltByDC } from './Branding';
+import { BuiltBy } from './Branding';
 import LanguageToggle from './LanguageToggle';
 
-function handleLogo(props) {
-  if (process.env.REACT_APP_API === 'ec') {
-    return (
-      <footer>
-        <img
-          className="footer-logo"
-          src={'https://ukelectoralcommission.files.wordpress.com/2014/03/logo2.png'}
-          alt="Electoral Commission Logo"
-          width={'100px'}
-        />
-      </footer>
-    );
-  } else {
-    return (
-      <footer>
-        {props.showLanguageToggle && <LanguageToggle {...props} />}
-        <BuiltByDC />
-      </footer>
-    );
-  }
-}
+// function handleLogo(props) {
+//   if (process.env.REACT_APP_BRAND === 'EC') {
+//     return (
+//       <footer>
+//         <img
+//           className="footer-logo"
+//           src={'https://ukelectoralcommission.files.wordpress.com/2014/03/logo2.png'}
+//           alt="Electoral Commission Logo"
+//           width={'100px'}
+//         />
+//       </footer>
+//     );
+//   } else {
+//     return (
+//       <footer>
+//         {props.showLanguageToggle && <LanguageToggle {...props} />}
+//         <BuiltBy />
+//       </footer>
+//     );
+//   }
+// }
 
 function Footer(props) {
-  return <footer>{handleLogo(props)}</footer>;
+  return (
+    <footer>
+      {props.showLanguageToggle && <LanguageToggle {...props} />}
+      <BuiltBy />
+    </footer>
+  );
 }
 
 export default Footer;
