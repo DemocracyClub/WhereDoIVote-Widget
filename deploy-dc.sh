3e2a6597643cbd4bffc27e60421624fd15c52f7d
Virginia Dooley <vcdooley@gmail.com>
2021-03-08 18:37:44 +0000

    create brand specific deploy

---
+++ deploy-dc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+die() {
+    echo >&2 "$@"
+    exit 1
+}
+
+if [ $# -eq 0 ]
+  then
+    die "No env supplied. Call with ./deploy-dc.sh [dev|prod]"
+fi
+
+CACHE_HEADERS="max-age=600"
+ACL="public-read"
+
+DEPLOY_ENV=$1
+[ $DEPLOY_ENV == "dev" ] || [ $DEPLOY_ENV == "prod" ] || die "Env not valid. Must be dev or prod"
+if [ "$DEPLOY_ENV" = "prod" ]; then
+    BUCKET="widget.wheredoivote.co.uk"
+elif [ "$DEPLOY_ENV" = "dev" ]; then
+    BUCKET="stagingwidget.wheredoivote.co.uk"
+else
+    die "Env not valid. Must be dev or prod"
+fi
+
+echo "Deploying to $DEPLOY_ENV: $BUCKET ..."
+
+
+deploy-to-s3() {
+    aws s3 cp --cache-control ${CACHE_HEADERS} --acl ${ACL} $1 s3://${BUCKET}/$2
+}
+
+rm -rf build
+
+JS_FILE=$(cat build/asset-manifest.json | jq -r '.files."main.js"')
+
+deploy-to-s3 ./build/${JS_FILE} dc_wdiv.js
+deploy-to-s3 ./demo.html demo.html
+deploy-to-s3 ./public/img/logo-with-text.png logo-with-text.png
