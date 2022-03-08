68d72c8b25430da6906d308b2938143c48e9afac
Virginia Dooley <vcdooley@gmail.com>
2022-03-08 17:34:00 +0000

    Cleanup unused code

--- src/Footer.js
+++ src/Footer.js
@@ -2,28 +2,6 @@ import React from 'react';
 import { BuiltBy } from './Branding';
 import LanguageToggle from './LanguageToggle';
 
-// function handleLogo(props) {
-//   if (process.env.REACT_APP_BRAND === 'EC') {
-//     return (
-//       <footer>
-//         <img
-//           className="footer-logo"
-//           src={'https://ukelectoralcommission.files.wordpress.com/2014/03/logo2.png'}
-//           alt="Electoral Commission Logo"
-//           width={'100px'}
-//         />
-//       </footer>
-//     );
-//   } else {
-//     return (
-//       <footer>
-//         {props.showLanguageToggle && <LanguageToggle {...props} />}
-//         <BuiltBy />
-//       </footer>
-//     );
-//   }
-// }
-
 function Footer(props) {
   return (
     <footer>
