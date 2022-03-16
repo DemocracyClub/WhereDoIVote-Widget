267a375002105db7616849bd4add3f817ed43dbb
Sym Roe <sym.roe@talusdesign.co.uk>
2022-03-16 15:05:48 +0000

    Fix comment

--- .github/write-comment.sh
+++ .github/write-comment.sh
@@ -4,8 +4,8 @@ cat > .github/workflows/deployment_comment.md <<EOF
 
 This PR has been deployed.
 
-* [EC widget]($PUBLIC_URL$GITHUB_HEAD_REF/ec/index.html)
-* [DC widget]($PUBLIC_URL$GITHUB_HEAD_REF/dc/index.html)
+* [EC widget]($PUBLIC_URL/$GITHUB_HEAD_REF/ec/index.html)
+* [DC widget]($PUBLIC_URL/$GITHUB_HEAD_REF/dc/index.html)
 
 
 EOF
