a7c8dd1127429bf2bf19ad78dd3110441d8723c8
Virginia Dooley <vcdooley@gmail.com>
2024-06-20 20:33:21 +0100

    Fix conditional for uses_party_lists

--- src/Candidates.js
+++ src/Candidates.js
@@ -5,6 +5,7 @@ import { injectIntl } from 'react-intl';
 
 function Candidates(props) {
   const ballot = props.ballot;
+  const uses_party_lists = ballot.voting_system.uses_party_lists;
   const candidatesVerified = ballot.candidates.length > 1 && ballot.candidates_verified;
   const { formatMessage } = props.intl;
   return (
@@ -12,7 +13,7 @@ function Candidates(props) {
       {candidatesVerified && !ballot.cancelled && (
         <section className="Candidates" data-testid="candidates">
           <h3>{formatMessage({ id: 'elections.candidates_heading' })}</h3>
-          {props.ballot.voting_system.uses_party_lists === true ? (
+          {uses_party_lists ? (
             <PartyList {...props} />
           ) : (
             <CandidateList candidates={props.ballot.candidates} />
