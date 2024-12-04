967d7ba38ed027dfb0009bc6e4ae73e179c4a9f9
chris48s <git@chris-shaw.dev>
2024-12-04 16:31:51 +0000

    show correct opening times for City of London local elections

--- src/PollingStation.js
+++ src/PollingStation.js
@@ -23,19 +23,24 @@ function PollingStation(props) {
     splitAddress.push(<br key={index} />);
   });
   return (
-    <section className="PollingStation">
+    <section className="PollingStation" data-testid="station-found">
       <h2 className="eiw-secondary-header">
         <FormattedMessage id="station.your-station" description="Vote on Polling Day" />
       </h2>
       <address data-testid="address" className="address">
         <p>{splitAddress.slice(0, splitAddress.length - 1)}</p>
       </address>
-      <p>
-        <FormattedMessage
-          id="station.opening-hours"
-          description="Polling stations are open from 7am to 10pm on polling day."
-        />
-      </p>
+      {props.openingTimes && (
+        <p>
+          <FormattedMessage
+            id="station.opening-hours"
+            values={{
+              start: props.openingTimes.start,
+              end: props.openingTimes.end,
+            }}
+          />
+        </p>
+      )}
       {show_i18n_link && (
         <p>
           For detailed information on accessibility, see <a href={wdiv_link}>WhereDoIVote.co.uk</a>
