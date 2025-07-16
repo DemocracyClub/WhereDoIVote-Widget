5ebb4d1a492cf8420341a0d9cd5bce606313a60e
chris48s <git@chris-shaw.dev>
2025-07-16 16:36:00 +0100

    update isConstituency and isRegional for Senedd changes

--- src/Ballot.js
+++ src/Ballot.js
@@ -2,20 +2,35 @@ import React from 'react';
 import { injectIntl } from 'react-intl';
 import BallotInfo from './BallotInfo';
 
+function isConstituency(ballot_paper_id) {
+  if (ballot_paper_id.startsWith('senedd.r.')) {
+    return false;
+  }
+  if (
+    ['parl.', 'nia.', 'gla.c.', 'senedd.c.', 'sp.c.'].some((prefix) =>
+      ballot_paper_id.startsWith(prefix)
+    )
+  ) {
+    return true;
+  }
+  if (/^senedd\.[a-z-]+.\d{4}-\d{2}-\d{2}$/.test(ballot_paper_id)) {
+    return true;
+  }
+  return false;
+}
+
+function isRegion(ballot_paper_id) {
+  return ['senedd.r.', 'sp.r.'].some((prefix) => ballot_paper_id.startsWith(prefix));
+}
+
 function Ballot(props) {
   const ballot = props.ballot;
   const candidatesVerified = ballot.candidates.length > 1 && ballot.candidates_verified;
-  const isConstituency = ['parl.', 'nia.', 'gla.c.', 'senedd.c.', 'sp.c.'].some((prefix) =>
-    ballot.ballot_paper_id.startsWith(prefix)
-  );
-  const isRegion = ['senedd.r.', 'sp.r.'].some((prefix) =>
-    ballot.ballot_paper_id.startsWith(prefix)
-  );
   const isByElection = ballot.ballot_paper_id.includes('.by.');
   let divisionType = '';
-  if (isConstituency) {
+  if (isConstituency(ballot.ballot_paper_id)) {
     divisionType = 'Constituency';
-  } else if (isRegion) {
+  } else if (isRegion(ballot.ballot_paper_id)) {
     divisionType = 'Region';
   }
   let bySuffix = '';
@@ -42,3 +57,4 @@ function Ballot(props) {
 }
 
 export default injectIntl(Ballot);
+export { isConstituency, isRegion };
