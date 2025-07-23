8f5bb2047b53fc334b33d2272a9ce1a6ecb1f1fe
chris48s <git@chris-shaw.dev>
2025-07-23 10:17:23 +0100

    remove react-app-rewired

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-/* config-overrides.js */
-const rewireRawLoader = require('@baristalabs/react-app-rewire-raw-loader')
-
-module.exports = function override (config, env) {
-    config = rewireRawLoader(config, env)
-    return config
-}
-
