e846616f88133315a28d48d3746b369a475debb9
Erin Pinheiro <erin.manal@gmail.com>
2026-03-28 21:16:42 +0000

    force cy polyfill

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ import ReactDOM from 'react-dom';
 import ElectionInformationWidget from './ElectionInformationWidget';
 import cssVars from 'css-vars-ponyfill';
 
+/*
+Use @formatjs/intl-datetimeformat to polyfill Welsh locale
+pack for browsers that don't have it.
+For example, Chrome doesn't ship with cy locale by default.
+
+If we need to add number formatting in future,
+we will need to add @formatjs/intl-numberformat here
+*/
+import '@formatjs/intl-datetimeformat/polyfill-force.js';
+import '@formatjs/intl-datetimeformat/locale-data/en';
+import '@formatjs/intl-datetimeformat/locale-data/cy';
+
 if (process.env.NODE_ENV !== 'production') {
   var axe = require('@axe-core/react');
   axe(React, ReactDOM, 1000);
